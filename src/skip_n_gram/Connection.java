b9c69c13de1b8af9379d7eec5b74fc40f07cd2f6
Giuliana Marquesi <giugianam@gmail.com>
2016-11-25 19:02:12 -0200

    deletado o blueprints, os jars necessarios tao na pasta /opt/orientdb/lib

--- src/skip_n_gram/Connection.java
+++ src/skip_n_gram/Connection.java
@@ -1,32 +1,8 @@
 package skip_n_gram;
 
-import com.tinkerpop.blueprints.Edge;
-import com.tinkerpop.blueprints.Vertex;
-import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 
 public class Connection {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		OrientGraph graph = new OrientGraph("remote:localhost:2424/skip_n_gram", "admin", "admin");
-		
-		try {
-			Vertex luca = graph.addVertex(null); // 1st OPERATION: IMPLICITLY
-													// BEGIN A TRANSACTION
-			luca.setProperty("name", "Luca");
-			Vertex marko = graph.addVertex(null);
-			marko.setProperty("name", "Marko");
-			Edge lucaKnowsMarko = graph.addEdge(null, luca, marko, "knows");
-			graph.commit();
-		} catch (Exception e) {
-			graph.rollback();
-		}
-
-		graph.shutdown();
-
-	}
+	
 
 }
