80c4fad3695e55a1b3cd562c33762d1a711e5359
Giuliana Marquesi <giugianam@gmail.com>
2016-11-25 19:16:57 -0200

    finalmente a conexao foi. fiz o basico do basico. ainda nao testei no orientdb...

--- src/skip_n_gram/Connection.java
+++ src/skip_n_gram/Connection.java
@@ -1,8 +1,25 @@
 package skip_n_gram;
 
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 
 public class Connection {
 
-	
+	public static void main(String args[]){
+		
+		OrientGraph graph = new OrientGraph("remote:localhost:2424/skip_n_gram");
+		try {
+			Vertex luca = graph.addVertex(null); // 1st OPERATION: IMPLICITLY BEGINS TRANSACTION
+			luca.setProperty( "name", "Luca" );
+			Vertex marko = graph.addVertex(null);
+			marko.setProperty( "name", "Marko" );
+			Edge lucaKnowsMarko = graph.addEdge(null, luca, marko, "knows");
+			graph.commit();
+			
+		} finally {
+		  graph.shutdown();
+		}
+	}
 
 }
