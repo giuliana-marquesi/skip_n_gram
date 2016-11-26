9e7fc7c4f685b480cb349729ad3243f5f911bf1e
ViniciusBaptista <vbaptista7@gmail.com>
2016-11-26 01:27:44 -0200

    Alterando classe 'padrão' para fazer mais testes

--- src/skip_n_gram/Connection.java
+++ src/skip_n_gram/Connection.java
@@ -1,25 +1,86 @@
 package skip_n_gram;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 
 public class Connection {
-
-	public static void main(String args[]){
-		
-		OrientGraph graph = new OrientGraph("remote:localhost:2424/skip_n_gram");
+	private HashMap<String, Integer> estatistica;
+	OrientGraph graph = new OrientGraph("remote:localhost:2424/skip_n_gram","root", "root");
+	Vertex lastWord;
+	Vertex nextWord;
+	
+	public Connection(){
+		estatistica = new HashMap<String, Integer>();
+	}
+	 
+	
+	public void lerArquivo(String nome){
+		File file = new File(nome);
+		Scanner scanner = null;
 		try {
-			Vertex luca = graph.addVertex(null); // 1st OPERATION: IMPLICITLY BEGINS TRANSACTION
-			luca.setProperty( "name", "Luca" );
-			Vertex marko = graph.addVertex(null);
-			marko.setProperty( "name", "Marko" );
-			Edge lucaKnowsMarko = graph.addEdge(null, luca, marko, "knows");
+			scanner = new Scanner(file);
+			int i = 0;
+			while( scanner.hasNext() ){
+				System.out.println(i++);
+				atualizarDicionario(scanner.next());
+			}
 			graph.commit();
+			graph.shutdown();			
+		}catch(Exception e){
+			System.out.println("dançou mano!");
 			
-		} finally {
-		  graph.shutdown();
+		}finally{
+			if(scanner !=null){
+				scanner.close();
+			}
 		}
+	}
+	
+	private void atualizarDicionario(String str) {
+		//String labelForEdge = this.lastWord.getProperty("word") + "--" + str;
+		//System.out.println(str + ", ");
+		try {
+			this.nextWord = graph.addVertex(null);
+			this.nextWord.setProperty("word", str);
+			
+			//Edge lastWord_newWord = graph.addEdge(null, this.lastWord, this.nextWord, "arestra");
+			
+			//this.lastWord = this.nextWord;
+			
+			
+		} catch (Exception e) {
+			System.out.println("e" + e);
+			//graph.rollback();
+		}
+	}
+	
+	public int qtdPalavrasUnicas(){
+		return estatistica.size();
+	}
+
+	public void mostrarQtd() {
+		for (String key: estatistica.keySet()) {
+		    System.out.println("key : " + key);
+		    System.out.println("value : " + estatistica.get(key));
+		}
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Connection a = new Connection();
+		//String nome = "/home/aluno/teste.txt";
+		String nome = "/home/vinicius/miniWiki.txt";
+		a.lerArquivo(nome);
+		//a.mostrarQtd();
+		System.out.println(a.qtdPalavrasUnicas());
+		
 	}
 
 }
