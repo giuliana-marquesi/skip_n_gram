8d0e52ca81fe5f2f5c233d0d1da51d69dac9b008
Giuliana <giugianam@gmail.com>
2016-11-29 00:34:22 -0200

    minha alteracoes, sõ caregaum vertice chamado properties

--- src/skip_n_gram/Arquivo.java
+++ src/skip_n_gram/Arquivo.java
@@ -1,5 +1,42 @@
 package src.skip_n_gram;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Arquivo {
-	private texto 
+	
+	File file;
+	Scanner scanner;
+	
+	public Arquivo(String nome){
+		this.file = new File(nome);
+	}
+	
+	public void lerArquivo(){
+		
+		Manipulador manipulador = new Manipulador();
+		
+		try {
+			scanner = new Scanner(file);
+			while( scanner.hasNext() ){
+				manipulador.verificaPoderIncluirNoBD(scanner.next());
+			}
+			
+		}catch(Exception e){
+			System.out.println("dançou mano!");
+			manipulador.desligaAConexao();
+			
+		}finally{
+			if(scanner !=null){
+				scanner.close();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome = "/home/usuario/Documentos/skip_n_gram/";
+		Arquivo arq = new Arquivo(nome);
+		arq.lerArquivo();
+	}
 }
--- src/skip_n_gram/Connection.java
+++ src/skip_n_gram/Connection.java
@@ -1,7 +1,5 @@
 package src.skip_n_gram;
 
-import com.tinkerpop.blueprints.Edge;
-import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 
 public class Connection {
@@ -17,19 +15,16 @@ public class Connection {
 		}
 	}
 
-	public static void main(String args[]){
+	public OrientGraph getConnectedGraph(){
 		
-		try {
-			Vertex luca = graph.addVertex(null); // 1st OPERATION: IMPLICITLY BEGINS TRANSACTION
-			luca.setProperty( "name", "Luca" );
-			Vertex marko = graph.addVertex(null);
-			marko.setProperty( "name", "Marko" );
-			Edge lucaKnowsMarko = graph.addEdge(null, luca, marko, "knows");
-			graph.commit();
-			
-		} finally {
-		  graph.shutdown();
-		}
+		return graph;
+		  
 	}
-
+	
+	public void closeConnection(){
+		graph.shutdown();
+	}
+	
 }
+
+
