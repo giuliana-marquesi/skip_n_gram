dcc501d53093a75357826e4b6c5fa271cb47a5d5
ViniciusBaptista <vbaptista7@gmail.com>
2016-12-08 00:30:17 -0200

    Adicionando regra skip_n_gram

--- src/palavras/Arquivo.java
+++ src/palavras/Arquivo.java
@@ -9,6 +9,13 @@ public class Arquivo {
 	
 	File file;
 	Scanner scanner;
+	int controladorDePulos = 0;
+	int controladorDeSelects = 0;
+	int qtdPulos = 10;
+	int qtdSelect = 1;
+	int bla = 0;
+	int meh = 0;
+	int i = 0;
 	
 	public Arquivo(String nome){
 		this.file = new File(nome);
@@ -22,21 +29,46 @@ public class Arquivo {
 		return palavra.length() > 0;
 	}
 	
+	private void completouOsPulos() {
+		if(controladorDePulos >= qtdPulos) {
+			controladorDePulos = 0;
+			controladorDeSelects = 0;
+		}
+	}
+	
+	private void fazerPulo() {
+		controladorDePulos++;
+		bla++;
+	}
+	
+	private void seleciona() {
+		controladorDeSelects++;
+		meh++;
+	}
+	
 	public void lerArquivo(){
 		
 		Manipulador manipulador = new Manipulador();
 		System.out.println("foi no ler arquivo");
 		
 		try {
+			// criar letrinhas: scanner = new Scanner(file).useDelimiter("[^\\p{javaDigit}\\p{javaLetter}]*");
 			scanner = new Scanner(file);
-			
 			System.out.println("Scaniou");
 			while(scanner.hasNext() ){
+				i++;
 				System.out.println("tem palavra");
 				String cleanWord = cleaningWord(scanner.next());
 				if(isWord(cleanWord)) {
-					manipulador.includesInSequence(cleanWord);
-				}				
+					if(controladorDeSelects < qtdSelect) {
+						seleciona();
+						//manipulador.includesInSequence(cleanWord);
+					} else {
+						fazerPulo();
+						completouOsPulos();
+					}
+				}
+				
 			}
 			
 		}catch(Exception e){
@@ -47,6 +79,9 @@ public class Arquivo {
 			if(scanner !=null){
 				scanner.close();
 			}
+			System.out.println("i " + i);
+			System.out.println("bla " + bla);
+			System.out.println("meh " + meh);
 		}
 	}
 }
